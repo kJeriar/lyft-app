c27a796a3c821253dc19e35f4c31f95107f44344
kJeriar <karla.jeria@gmail.com>
2017-07-03 13:45:55 -0400

    Update main.js
    funciones datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,31 @@
-function caracteresCorreoValido(email, div){
-    console.log(email);
-    //var email = $(email).val();
-    var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+$(document).ready(function() {
+	$("button").click(function() {
+		first_name();
+		last_name();
+		email();
+	}); 
 
-    if (caract.test(email) == false){
-        $(div).hide().removeClass('hide').slideDown('fast');
 
-        return false;
-    }else{
-        $(div).hide().addClass('hide').slideDown('slow');
-//        $(div).html('');
-        return true;
-    }
+function email(){
+var emailValue = $("#input-email").val();
+if (!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).test(emailValue)){
+$(".email-container").append('<span> formato de correo no valido</span>');
+$("#input-email").val("");
 }
+}
+
+function first_name(){
+    var nameValue = $("#name").val();
+			if (!(/^[A-Z][a-z]/).test(nameValue)){
+			$(".name-container").append('<span>Nombre inválido.</span>');
+			$("#name").val("");
+			}
+	}
+
+function last_name(){
+    var nameValue = $("#name").val();
+			if (!(/^[A-Z][a-z]/).test(nameValue)){
+			$(".name-container").append('<span>apellido inválido.</span>');
+			}
+	}
+});
