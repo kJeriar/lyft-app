a543c19b71cbf91095fa8365e7f3f0a3ebc375bb
kJeriar <karla.jeria@gmail.com>
2017-07-03 01:27:52 -0400

    Update main.js
    función para validar que el mail sea valido

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+unction caracteresCorreoValido(email, div){
+    console.log(email);
+    //var email = $(email).val();
+    var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+
+    if (caract.test(email) == false){
+        $(div).hide().removeClass('hide').slideDown('fast');
+
+        return false;
+    }else{
+        $(div).hide().addClass('hide').slideDown('slow');
+//        $(div).html('');
+        return true;
+    }
+}
+
