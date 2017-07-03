68454c657a2cde863b7cfc9c01d91ca7934fa363
kJeriar <karla.jeria@gmail.com>
2017-07-03 18:10:47 -0400

    Update main.js
    validaciones con detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,23 +9,23 @@ $(document).ready(function() {
 function email(){
 var emailValue = $("#input-email").val();
 if (!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).test(emailValue)){
-$(".email-container").append('<span> formato de correo no valido</span>');
+$(".email").append('<span> formato de correo no valido</span>');
 $("#input-email").val("");
 }
 }
 
 function first_name(){
-    var nameValue = $("#name").val();
+    var nameValue = $("#first_name").val();
 			if (!(/^[A-Z][a-z]/).test(nameValue)){
-			$(".name-container").append('<span>Nombre inválido.</span>');
-			$("#name").val("");
+			$(".first_name").append('<span>Nombre inválido.</span>');
+			$("#first_name").val("");
 			}
 	}
 
 function last_name(){
-    var nameValue = $("#name").val();
+    var nameValue = $("#last_name").val();
 			if (!(/^[A-Z][a-z]/).test(nameValue)){
-			$(".name-container").append('<span>apellido inválido.</span>');
+			$(".last_name").append('<span>apellido inválido.</span>');
 			}
 	}
 });
